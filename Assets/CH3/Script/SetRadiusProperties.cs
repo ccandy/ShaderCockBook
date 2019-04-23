6b71551099c3c97eae13ab903516bcd21fc0fef7
ccandy <ccunandy2001@gmail.com>
2019-04-23 11:12:03 +0800

    finish ch3

---
+++ Assets/CH3/Script/SetRadiusProperties.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class SetRadiusProperties : MonoBehaviour {
+
+    // Use this for initialization
+
+
+    public Material radiusMaterial;
+    public float    radius          = 1;
+    public Color    color           = Color.white;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(radiusMaterial!= null)
+        {
+            radiusMaterial.SetVector("_Center",     transform.position);
+            radiusMaterial.SetFloat("_Radius",      radius);
+            radiusMaterial.SetColor("_RadiusColor", color);
+        }
+	}
+}
